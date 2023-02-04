15dd8e20394ac1f04cd8b27febcb6c285e1ddcc7
Abarkel <ahmedbarkal96@gmail.com>
2023-02-04 22:34:52 +0100

    Exame4

---
+++ module4-solution/JS/module4.js
@@ -0,0 +1,17 @@
+(function() {
+    var names = ["Yaakov","Ahlam", "John", "Jen","Ahmed","Samer", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        console.log("Goodbye" + " "+ names[i])
+      } 
+      else
+      {
+        console.log("Hello" + " "+ names[i])
+      }
+    }
+  })();
+  
+  
+ 
+  
